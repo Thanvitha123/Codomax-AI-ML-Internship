5a3cb5f3e0629c35a44568becaa78416a42321da
Thanvitha H M <thanvithat83@gmail.com>
2026-08-07 18:49:58 +0530

    Update lists.py

--- Module-1/Python-Programs/lists.py
+++ Module-1/Python-Programs/lists.py
@@ -1,1 +1,20 @@
+# Program to demonstrate Lists in Python
 
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print("Fruit List:", fruits)
+
+print("First Fruit:", fruits[0])
+
+fruits.append("Grapes")
+
+print("Updated List:", fruits)
+
+print("Number of Fruits:", len(fruits))
+
+Output:
+
+Fruit List: ['Apple', 'Banana', 'Mango', 'Orange']
+First Fruit: Apple
+Updated List: ['Apple', 'Banana', 'Mango', 'Orange', 'Grapes']
+Number of Fruits: 5
