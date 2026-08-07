80aa50935b482cc2d0a14f890fce679d656c2770
Thanvitha H M <thanvithat83@gmail.com>
2026-08-07 18:45:37 +0530

    Update loops.py

--- Module-1/Python-Programs/loops.py
+++ Module-1/Python-Programs/loops.py
@@ -1,1 +1,30 @@
+# Program to demonstrate Loops in Python
 
+print("For Loop:")
+
+for i in range(1, 6):
+    print(i)
+
+print("\nWhile Loop:")
+
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+
+
+Output:
+For Loop:
+1
+2
+3
+4
+5
+
+While Loop:
+1
+2
+3
+4
+5
