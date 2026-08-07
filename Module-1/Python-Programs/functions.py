54b9de3591e30da8b344901bdde3de996ad6b395
Thanvitha H M <thanvithat83@gmail.com>
2026-08-07 18:47:12 +0530

    Update functions.py

--- Module-1/Python-Programs/functions.py
+++ Module-1/Python-Programs/functions.py
@@ -1,1 +1,17 @@
+# Program to demonstrate Functions in Python
 
+def greet(name):
+    print("Hello,", name + "!")
+
+def add(a, b):
+    return a + b
+
+greet("Thanvitha")
+
+result = add(10, 20)
+print("Sum =", result)
+
+Output:
+
+Hello, Thanvitha!
+Sum = 30
