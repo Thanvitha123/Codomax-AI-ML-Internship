ea2cadfd0f9a395dc75bb8c98a8d09d018ba9aa2
Thanvitha H M <thanvithat83@gmail.com>
2026-08-07 18:41:52 +0530

    Update variables.py

--- Module-1/Python-Programs/variables.py
+++ Module-1/Python-Programs/variables.py
@@ -1,1 +1,12 @@
 
+# Program to demonstrate Variables in Python
+
+name = "Thanvitha"
+age = 20
+cgpa = 9.53
+college = "PES Institute of Technology and Management"
+
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa)
+print("College:", college)
